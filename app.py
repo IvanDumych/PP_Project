c4e32b27d4a23b3e69fc01e4bd4690899ba9bd25
Vitalii Shkliarov <shkliarov.vitalik@gmail.com>
2020-11-29 22:31:22 +0200

    Change Project Structure

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/api/v1/hello-world-7')
-def index():
-    return 'Hello World-3'
+from pp_project import app
 
 
 if __name__ == "__main__":
