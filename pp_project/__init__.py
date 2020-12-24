dd355cf731f12299ffb5dc67b93cd995418ec800
Ostap <ostapbod674@gmail.com>
2020-12-24 15:25:12 +0200

    Auth system added

--- pp_project/__init__.py
+++ pp_project/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_bcrypt import Bcrypt
+from flask_httpauth import HTTPBasicAuth
 
 # init app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+auth = HTTPBasicAuth()
 
 # Database
 engine = create_engine('sqlite:///test.db', echo=True)
@@ -13,3 +15,5 @@ Session = sessionmaker(bind=engine)
 
 # bcrypt
 bcrypt = Bcrypt(app)
+
+import pp_project.urls
