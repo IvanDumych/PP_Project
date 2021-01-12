f436244c31e4727a8bf58b90ffee59f5351876a0
Vitalii Shkliarov <shkliarov.vitalik@gmail.com>
2021-01-12 20:55:45 +0200

    Unit tests

--- pp_project/__init__.py
+++ pp_project/__init__.py
@@ -3,14 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_bcrypt import Bcrypt
 from flask_httpauth import HTTPBasicAuth
+import config
 
 # init app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config.from_object('config.TestingConfig')
+# app.config.from_object('config.DevelopmentConfig')
 auth = HTTPBasicAuth()
 
 # Database
-engine = create_engine('sqlite:///test.db', echo=True)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
 Session = sessionmaker(bind=engine)
 
 # bcrypt
--- pp_project/hello_world.py
+++
@@ -1,7 +0,0 @@
-from pp_project import app
-
-
-@app.route('/')
-@app.route('/api/v1/hello-world-7')
-def index():
-    return 'Hello World-3'
